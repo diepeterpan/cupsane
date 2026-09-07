9bbcc2c4baf9869fd8274f8f4db8b693a48a6abc
Die Peter Pan <diepeterpan@gmail.com>
2026-09-07 20:51:20 +0200

    Change to magick

--- config/scanservjs.config.js
+++ config/scanservjs.config.js
@@ -4,6 +4,7 @@ const options = { paths: ['/usr/lib/scanservjs'] };
 module.exports = {
   afterConfig(config) {
     config.port = 8081;
+    config.convert = '/usr/bin/magick';
   },
   afterDevices(devices) {},  
   async afterScan(fileInfo) {},
